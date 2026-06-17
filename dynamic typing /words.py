00c914dde09f6e0f7ea5e3c06ffa043f099e954a
Manshi <manshih4x@gmail.com>
2026-06-17 19:15:41 +0530

    Add word list to words.py

---
+++ dynamic typing /words.py
@@ -0,0 +1,12 @@
+word_list = [
+    "python", "keyboard", "monitor", "window", "program",
+    "developer", "computer", "internet", "software", "coding",
+    "function", "variable", "project", "random", "string",
+    "number", "object", "class", "method", "package",
+    "library", "terminal", "button", "screen", "application",
+    "network", "system", "design", "testing", "performance",
+    "speed", "accuracy", "practice", "learning", "success",
+    "future", "challenge", "problem", "solution", "algorithm",
+    "binary", "search", "stack", "queue", "array",
+    "dictionary", "tuple", "debug", "compile", "execute"
+]
